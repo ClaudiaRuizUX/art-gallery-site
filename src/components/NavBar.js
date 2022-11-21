50e8b5603f5fcc8b82ed736efdf1a067f521e698
claudiaruiz <claudiaruizux@gmail.com>
2022-11-21 13:29:01 +1100

    fixes html for the navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,61 +36,53 @@ import {
   
     return (
       <div className="nav-container">
-        
-        <Navbar color="light" light expand="md">
-          <Container>
+        <Navbar bg="light" expand="lg" >
+          <Container >
             <NavbarBrand className="logo" />
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
               <Nav className="mr-auto" navbar>
                 <NavItem>
-                  <NavLink
-                    tag={RouterNavLink} to="/" exact activeClassName="router-link-exact-active">
+                  <NavLink tag={RouterNavLink} to="/" exact activeClassName="router-link-exact-active">
                     <a class="navbar-brand" href="/"><img src="../assets/logo-header.png" alt="ArtGallery"/></a>
                   </NavLink>
                 </NavItem>
-                <NavItem>
-                  <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                  </form>
-                </NavItem>
-                {isAuthenticated && (
-                  <NavItem>
-                    <NavLink
-                      tag={RouterNavLink}
-                      to="/external-api" exact activeClassName="router-link-exact-active">
-                      Projects Admin
-                    </NavLink>
-                  </NavItem>
-                )}
               </Nav>
-              <Nav className="d-none d-md-block" navbar>
-                {!isAuthenticated && (
-                  <NavItem>
-                    <Button id="qsLoginBtn" color="primary" className="btn-margin"
-                      onClick={() => loginWithRedirect()}>
-                      Log in
-                    </Button>
-                  </NavItem>
-                )}
-                {isAuthenticated && (
-                  <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret id="profileDropDown">
-                      <img src={user.picture} alt="Profile" className="nav-user-profile rounded-circle" width="50"/>
-                    </DropdownToggle>
-                    <DropdownMenu>
-                      <DropdownItem header>{user.name}</DropdownItem>
-                      <DropdownItem tag={RouterNavLink} to="/profile" className="dropdown-profile" activeClassName="router-link-exact-active">
-                        <FontAwesomeIcon icon="user" className="mr-3" /> Profile
-                      </DropdownItem>
-                      <DropdownItem id="qsLogoutBtn" onClick={() => logoutWithRedirect()} >
-                        <FontAwesomeIcon icon="power-off" className="mr-3" /> Log out
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                )}
-              </Nav>
+              <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Nav className="mr-auto" navbar>
+          {!isAuthenticated && (
+            <NavItem>
+              <Button id="qsLoginBtn" color="primary" className="btn-margin"
+                onClick={() => loginWithRedirect()}>
+                Log in
+              </Button>
+            </NavItem>
+          )}
+          {isAuthenticated && (
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret id="profileDropDown">
+                <img src={user.picture} alt="Profile" className="nav-user-profile rounded-circle" width="50"/>
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>{user.name}</DropdownItem>
+                <DropdownItem tag={RouterNavLink} to="/profile" className="dropdown-profile" activeClassName="router-link-exact-active">
+                  <FontAwesomeIcon icon="user" className="mr-3" /> Profile
+                </DropdownItem>
+                <DropdownItem id="qsLogoutBtn" onClick={() => logoutWithRedirect()} >
+                  <FontAwesomeIcon icon="power-off" className="mr-3" /> Log out
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          )}
+          </Nav>
             </Collapse>
           </Container>
         </Navbar>
