b274f36e96360c66f13333a422ce12f7f21bae3e
claudiaruiz <claudiaruizux@gmail.com>
2022-11-22 09:00:36 +1100

    setups basic style for product details page

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Button } from "reactstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import { ArrowRight } from 'react-bootstrap-icons';
+import { blue } from '@material-ui/core/colors';
+
 const ProjectPage = () => {
     return (
-        <div>
-            <div>Back</div>
-            <h1>Project Title</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    <div className="container col-md-8">
+        <div className="row my-5">
+        <Button className="layout-left" color="tertiary"> « Back </Button>
+            <h3>Project Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
             </p>
-            <ul>
-                <li>Warm</li>
-                <li>Gestural</li>
+            <ul className="pills">
+                <li><a href="">Cold</a> <a href="">X</a></li>
+                <li><a href="">Cold</a></li>
+                <li><a href="">Gestural</a></li>
+                <Button color="tertiary">+ Add Category</Button>
             </ul>
-            <div className="card-image">  
-                <img src="/" alt="image" />
-            </div>
         </div>
+    </div>
     );
 }
 export default ProjectPage;
