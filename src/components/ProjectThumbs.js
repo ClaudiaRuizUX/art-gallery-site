9d6c30003ea5e7911a3c7ce1cd678d0f5d551bca
claudiaruiz <claudiaruizux@gmail.com>
2022-11-24 11:09:34 +1100

    connects ProjectThumb to HomePage with layout and dynamic content

--- src/components/ProjectThumbs.js
+++ src/components/ProjectThumbs.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Container } from "reactstrap";
+// styles & fonts
+import { Button } from "reactstrap";
+import { ArrowRight } from 'react-bootstrap-icons';
+import { blue } from '@material-ui/core/colors';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ProjectThumbs = (props) => {
-    const { title} = props;
+    const { title, image} = props;
     return (
-        <>
-        <div className='col-md-4 py-3'>
+        <div className='col-md-4 py-3'>  
             <div className="card">
+                <img className="card-img-top mx-auto" src={image} alt="350x350"/>
                 <div className="card-body">
                     <h5 className="card-title text-center">{title}</h5>
                 </div>
             </div>
         </div>
-        </>  
     );    
 };  
 export default ProjectThumbs;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,31 +3,46 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { connect } from "react-redux";
 import { fetchProjects} from "../actions/projectActions";
 import ProjectThumbs from '../components/ProjectThumbs';
-
+// styles & fonts
 import { Button } from "reactstrap";
 import { ArrowRight } from 'react-bootstrap-icons';
 import { blue } from '@material-ui/core/colors';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class HomePage extends Component {
     componentDidMount() {
       this.props.fetchProjects();
     }
-    
     render() {
         return (
           <>
-            <div className="App">
-              <h1>Art Projects</h1>
-              {this.props.loading ?
-                <h1>LOADING...</h1> : 
-                <div className="col s8 cards-container">
-                  {this.props.projects.map((project =>
-                      <ProjectThumbs title={project.title} />
-                      ))
-                  }
-                </div>
-              } 
-            </div>
+          <section className="Branding-Left">
+            <h1>Art<span>Gallery</span></h1>
+            <p className="slogan">Find Art by Themes or Styles</p>
+          </section>
+          <section className="my-5">
+            <div className="d-flex justify-content-between"> 
+                  <h3>Abstract Art</h3>
+                  <Button  color="secondary">✐ Edit Section </Button>
+              </div>
+              <ul className="pills">
+                  <li><a className="active" href="">All</a></li>
+                  <li><a href="">Geometric</a> <a href="">X</a></li>
+                  <li><a href="">Cold</a></li>
+                  <li><a href="">Warm</a></li>
+                  <li><a href="">Gestural</a></li>
+                  <Button color="tertiary">+ Add Category</Button>
+              </ul>
+              <div className="container">
+                  {this.props.loading ?
+                    <h1>LOADING...</h1> : 
+                    <div className="row">  
+                      {this.props.projects.map((project => <ProjectThumbs title={project.title} image={project.image}/>))} 
+                    </div>
+                  } 
+
+              </div>
+          </section>
           </>
         );
       }
